18a66b56e785e4a4108f62cb48208d2d74320c21
Hermanto <45707088+WIWIWIAA@users.noreply.github.com>
2025-05-26 14:40:45 +0930

    figure out router class
    what we need based on topology logic.
    
    distance table to list out distance of each routers
    
    neigbors for direct distance
    
    and all routers lists

--- distance_vector.py
+++ distance_vector.py
@@ -1,4 +1,19 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
+
+class Router:
+    def __init__(self, name, all_routers):
+        self.name = name                    
+        self.all_routers = all_routers      
+        self.neighbors = {}                 
+        self.distance_table = {}            
+        
+        # Initialize distance table
+        for destination in all_routers:
+            if destination != self.name:
+                self.distance_table[destination] = {}
+                for next_hop in all_routers:
+                    if next_hop != self.name:
+                        self.distance_table[destination][next_hop] = float('inf')
 
 def main():
     # This is where your program starts
